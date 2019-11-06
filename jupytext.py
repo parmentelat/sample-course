dde9fd45dda2e0a8abeb700968a46b40eecc471b
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2019-11-06 12:43:54 +0100

    added a jupytext-enabled notebook as a py file

---
+++ jupytext.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all
+#     notebook_metadata_filter: all,-language_info
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 1.2.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# ***NOTE***
+#
+# ce notebook est le premier de ceux qui utilisent un **workflow basé sur jupytext**
+#
+# en pratique ça signifie que
+# * dans mon repo de travail je sauve automatiquement un `.ipynb` **ET** un `.py` 
+# * je ne mets **sous git QUE le `.py`** 
+#
+# mon objectif est de vérifier que nbhosting fonctionne correctement avec de tels notebooks  
+# **update**: ça ne fonctionne pas en l'état sous nbhosting; 
+# mais j'hésite à faire la correction pour la raison suivante. 
+#
+#
+#
+# les avantages :
+# * `git diff` plus facile (pas besoin de nbdime en fait)
+# * `git commit` plus facile (pas besoin de nbstripout non plus)
+# * `git pull` plus facile (jupytext est configuré pour **ne pas sauver certains détails** comme notamment le niveau de version de Python qui n'arrête pas de changer entre moi et les différentes images sur nbhosting)
+#
+# à suivre sur la durée donc
+
+# %%
+def fact(n):
+    return 1 if n <= 1 else n * fact(n-1)
+
+
+# %%
+fact(12)
